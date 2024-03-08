353a5a21e43dc87f43125521ff33dc57f767fe75
Dexter Zou <jiaxiangzou@microsoft.com>
2024-03-08 16:19:13 +0800

    add git scripts

---
+++ Win_ReinstallGit.ps1
@@ -0,0 +1,16 @@
+$uninstaller = Get-ChildItem "C:\Program Files\Git\unins*.exe" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+
+if ($uninstaller) {
+    Write-Output "Git is installed. Uninstalling..."
+    Start-Process -FilePath $uninstaller -ArgumentList "/SILENT" -Wait
+}
+
+# Install Git
+$version = $args[0]
+if ($version) {
+    Write-Output "Installing Git version $version..."
+    winget install --id Git.Git -e --source winget --version $version
+} else {
+    Write-Output "Installing latest Git version..."
+    winget install --id Git.Git -e --source winget
+}
