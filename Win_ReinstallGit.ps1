9eba397ca94192b307a63c0bf264a3de17fa9912
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-13 17:35:44 +0800

    Update Win_ReinstallGit.ps1

--- Win_ReinstallGit.ps1
+++ Win_ReinstallGit.ps1
@@ -1,20 +1,25 @@
-$uninstaller = Get-ChildItem "C:\Program Files\Git\unins*.exe" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+$options = @("  1) Git with VFS", "  2) Latest Git")
+Write-Host "Choose an option:"
+for ($i=0; $i -lt $options.Count; $i++) {
+    Write-Host "$($i+1). $($options[$i])"
+}
+$choice = Read-Host "Enter 1 or 2 or Specific Git Version: "
 
+$uninstaller = Get-ChildItem "C:\Program Files\Git\unins*.exe" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
 if ($uninstaller) {
     Write-Output "Git is installed. Uninstalling..."
     Start-Process -FilePath $uninstaller -ArgumentList "/SILENT" -Wait
 }
 
 # Install Git
-$version = $args[0]
-if ($version -eq "vfs") {
+if ($choice -eq "1") {
     Write-Output "Installing VFS Git ..."
     winget install --id Microsoft.Git
     winget install --id Microsoft.VFSforGit
-} elseif ($version) {
-    Write-Output "Installing Git version $version..."
-    winget install --id Git.Git -e --source winget --version $version
-} else {
+} elseif ($choice -eq "2") {
     Write-Output "Installing latest Git version..."
     winget install --id Git.Git -e --source winget
+} else {
+    Write-Output "Installing Git version $choice..."
+    winget install --id Git.Git -e --source winget --version $choice
 }
