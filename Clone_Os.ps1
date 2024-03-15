f569c701a2a0aef8324fc6728fba69e540d44454
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-15 10:57:26 +0800

    Update Clone_Os.ps1

--- Clone_Os.ps1
+++ Clone_Os.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 
 $dst = $args[0]
+$repoCacheServer = $null
 
 while ($true) {
     $options = @("os.2020", "os", "OSClient", "XS.SDX.Settings")
@@ -12,18 +13,18 @@ while ($true) {
     if ($choice -eq "1") {
         $repo = "https://microsoft.visualstudio.com/OS/_git/os.2020"
         $repoCacheServer = "https://osgvfsserver.corp.microsoft.com/0d54b6ef-7283-444f-847a-343728d58a4d"
-        break
     } elseif ($choice -eq "2") {
         $repo = "https://microsoft.visualstudio.com/OS/_git/os"
         $repoCacheServer = "https://osgvfsserver.corp.microsoft.com/7bc5fd9f-6098-479a-a87e-1533d288d438"
-        break
     } elseif ($choice -eq "3") {
         $repo = "https://microsoft.visualstudio.com/DefaultCollection/OS/_git/OSClient"
-        break
     } elseif ($choice -eq "4") {
         $repo = "https://microsoft.visualstudio.com/DefaultCollection/Universal%20Store/_git/XS.SDX.Settings"
-        break
+    } else {
+        continue
     }
+
+    break
 }
 
 $international = $false
