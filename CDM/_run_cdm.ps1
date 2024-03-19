e6a8f893db01f918ab26ede33c8e041a0417527e
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-19 17:18:32 +0800

    Update _run_cdm.ps1

--- CDM/_run_cdm.ps1
+++ CDM/_run_cdm.ps1
@@ -5,26 +5,31 @@ for ($i=0; $i -lt $options.Count; $i++) {
 }
 $choice = Read-Host "Enter option index"
 
-if ($choice -eq "1") {
-    $cdmFolder = [System.Environment]::GetFolderPath('UserProfile')
-    $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "AppData\Local\Packages"
-    $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy"
-    $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "LocalState\ContentManagementSDK\Creatives"
-    Invoke-Item $cdmFolder
-} elseif ($choice -eq "2") {
-    regjump.exe "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\IrisService\Cache"
-} elseif ($choice -eq "3") {
-    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
-    $regName = "TestFlightId"
-    $regValue = "IX:00000000"
-    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion" -Name "IrisService" -Force
-    if (-not (Test-Path -Path "$regPath\$regName")) {
-        New-ItemProperty `
-            -Path $regPath `
-            -Name $regName `
-            -Value $regValue `
-            -PropertyType String `
-            -Force
+try {
+    if ($choice -eq "1") {
+        $cdmFolder = [System.Environment]::GetFolderPath('UserProfile')
+        $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "AppData\Local\Packages"
+        $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy"
+        $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "LocalState\ContentManagementSDK\Creatives"
+        Invoke-Item $cdmFolder
+    } elseif ($choice -eq "2") {
+        regjump.exe "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\IrisService\Cache"
+    } elseif ($choice -eq "3") {
+        $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
+        $regName = "TestFlightId"
+        $regValue = "IX:00000000"
+        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion" -Name "IrisService" -Force
+        if (-not (Test-Path -Path "$regPath\$regName")) {
+            New-ItemProperty `
+                -Path $regPath `
+                -Name $regName `
+                -Value $regValue `
+                -PropertyType String `
+                -Force
+        }
+        regjump.exe "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
     }
-    regjump.exe "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
+} catch {
+    Write-Error "An exception occurred: $_.Exception.Message"
 }
+
