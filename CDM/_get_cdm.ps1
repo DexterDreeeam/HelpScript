81eb4d174ec59dc5c2d78e23def620442d14f49a
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-19 16:08:43 +0800

    Update _get_cdm.ps1

--- CDM/_get_cdm.ps1
+++ CDM/_get_cdm.ps1
@@ -9,6 +9,23 @@ function RemoveFile($path) {
     Start-Process powershell -ArgumentList "Remove-Item `"$path`" -Force"
 }
 
+function ElevateLevel([string] $scriptFilePathAndArguments)
+{
+    $currentIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+    $currentPrincipal = New-Object System.Security.Principal.WindowsPrincipal($currentIdentity);
+    $administratorRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+    $isElevated = $currentPrincipal.IsInRole($administratorRole);
+    if (-not $isElevated)
+    {
+        Write-Host -ForegroundColor Cyan "This script requires administrator privileges. Elevating..."
+        Write-Host -ForegroundColor Cyan "$scriptFilePathAndArguments"
+        Start-Process -FilePath Powershell.exe -Verb RunAs -ArgumentList "-ExecutionPolicy Unrestricted -NoProfile -File ""$scriptFilePathAndArguments"" -PauseOnCompletion"
+        exit
+    }
+}
+
+ElevateLevel $MyInvocation.MyCommand.Path
+
 $repo = "https://raw.githubusercontent.com/dexterdreeeam/HelpScript/main/"
 $cdUrl = "$repo/CDM/_run_cdm.ps1"
 $cdPath = Join-Path -Path $pwd -ChildPath "_run_cdm.ps1"
@@ -25,4 +42,4 @@ RemoveFile $cdPath
 RemoveFile $rjPath
 
 # Delete Self
-RemoveFile($MyInvocation.MyCommand.Path)
+RemoveFile $MyInvocation.MyCommand.Path
