5929e332e497064a9f5d2904d18bc20f437efbeb
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-19 15:42:58 +0800

    update

--- CDM/_get_cdm.ps1
+++ CDM/_get_cdm.ps1
@@ -1,14 +1,28 @@
+function RemoveFile($path) {
+    Start-Process powershell -ArgumentList "Remove-Item `"$path`" -Force"
+}
+
 $repo = "https://raw.githubusercontent.com/dexterdreeeam/HelpScript/main/"
 $cdUrl = "$repo/CDM/_run_cdm.ps1"
 $cdPath = Join-Path -Path $pwd -ChildPath "_run_cd.ps1"
+$rjUrl = "$repo/CDM/regjump.exe"
+$rjPath = Join-Path -Path $pwd -ChildPath "regjump.exe"
 
 $cdExist = Test-Path $cdPath -PathType Leaf
 if (-not $cdExist) {
     Invoke-WebRequest -Uri $cdUrl -OutFile $cdPath
 }
 
-& $cdPath $args
+$rjExist = Test-Path $rjPath -PathType Leaf
+if (-not $rjExist) {
+    Invoke-WebRequest -Uri $rjUrl -OutFile $rjPath
+}
+
+Start-Process powershell -ArgumentList "-Command `"$args`"" -Verb RunAs
+
+# Delete Resources
+RemoveFile($cdPath)
+RemoveFile($rjPath)
 
 # Delete Self
-$myPsPath = $MyInvocation.MyCommand.Path
-Start-Process powershell -ArgumentList "Remove-Item `"$myPsPath`" -Force"
+RemoveFile($MyInvocation.MyCommand.Path)
--- CDM/_run_cdm.ps1
+++ CDM/_run_cdm.ps1
@@ -1,4 +1,4 @@
-$options = @("Open CDM content folder")
+$options = @("Open CDM content", "Open CDM Lite content")
 Write-Host "Choose an option:"
 for ($i=0; $i -lt $options.Count; $i++) {
     Write-Host "  $($i+1). $($options[$i])"
@@ -11,6 +11,22 @@ if ($choice -eq "1") {
     $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy"
     $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "LocalState\ContentManagementSDK\Creatives"
     Invoke-Item $cdmFolder
+} elseif ($choice -eq "2") {
+    $registryPath = "HKEY_CURRENT_USER\Software\Microsoft\Windows"
+    Start-Process -FilePath "regedit.exe" -ArgumentList "/e, $registryPath"
+
+    function jumpReg ($registryPath)
+    {
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion" `
+                         -Name "LastKey" `
+                         -Value $registryPath `
+                         -PropertyType String `
+                         -Force
+    
+        regedit
+    }
+    
+    jumpReg ("Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run") | Out-Null
 }
 
 # Delete Self
