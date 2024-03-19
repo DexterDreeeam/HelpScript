517159859aaaba9865e3aa443a0ccf4e9d6259dd
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-19 17:02:09 +0800

    Update _run_cdm.ps1

--- CDM/_run_cdm.ps1
+++ CDM/_run_cdm.ps1
@@ -1,4 +1,4 @@
-$options = @("Open CDM content", "Open CDM Lite content")
+$options = @("Open CDM content", "Open CDM Lite content", "Open CDM Lite TestFlight")
 Write-Host "Choose an option:"
 for ($i=0; $i -lt $options.Count; $i++) {
     Write-Host "  $($i+1). $($options[$i])"
@@ -12,23 +12,19 @@ if ($choice -eq "1") {
     $cdmFolder = Join-Path -Path $cdmFolder -ChildPath "LocalState\ContentManagementSDK\Creatives"
     Invoke-Item $cdmFolder
 } elseif ($choice -eq "2") {
-    $registryPath = "HKEY_CURRENT_USER\Software\Microsoft\Windows"
-    Start-Process -FilePath "regedit.exe" -ArgumentList "/e, $registryPath"
-
-    function jumpReg ($registryPath)
-    {
-        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion" `
-                         -Name "LastKey" `
-                         -Value $registryPath `
-                         -PropertyType String `
-                         -Force
-    
-        regedit
+    regjump.exe "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\IrisService\Cache"
+} elseif ($choice -eq "3") {
+    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
+    $regName = "TestFlightId"
+    $regValue = "IX:00000000"
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion" -Name "IrisService" -Force
+    if (-not (Test-Path -Path "$regPath\$regName")) {
+        New-ItemProperty `
+            -Path $regPath `
+            -Name $regName `
+            -Value $regValue `
+            -PropertyType String `
+            -Force
     }
-    
-    jumpReg ("Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run") | Out-Null
+    regjump.exe "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\IrisService"
 }
-
-# Delete Self
-$myPsPath = $MyInvocation.MyCommand.Path
-Start-Process powershell -ArgumentList "Remove-Item `"$myPsPath`" -Force"
