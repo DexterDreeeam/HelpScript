3861f758e718908d678ef6642630e250e53a88e1
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-03-30 22:43:50 +0800

    Update Install_VsRemoteTool.ps1

--- Install_VsRemoteTool.ps1
+++ Install_VsRemoteTool.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 $binary = "RemoteTools.amd64ret.enu.exe"
 $binaryPath = Join-Path -Path $pwd -ChildPath $binary
 $downloadUrl = "https://aka.ms/vs/17/release/" + $binary
-Invoke-WebRequest $downloadUrl -OutFile $binaryPath
+Start-BitsTransfer -Source $downloadUrl -Destination $binaryPath
 
 Start-Process -FilePath $binaryPath
 
