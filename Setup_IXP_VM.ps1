6942e9518c8f42a375de2727f733ad4b7bbbede4
dexterdreeeam <dexterdreeeam@foxmail.com>
2024-04-21 10:36:44 +0800

    Update Setup_IXP_VM.ps1

--- Setup_IXP_VM.ps1
+++ Setup_IXP_VM.ps1
@@ -149,7 +149,8 @@ function MainEntry {
             -KdSetupMode Disable `
             -VmNumProcessors 8 `
             -VmMemInGb 8 `
-            -VhdSource $selectedVhd
+            -VhdSource $selectedVhd `
+            -AllowOffline
     }
     else {
         net use \\ntdev\release
